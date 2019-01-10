39cae26f39887c079d6914fea08c5b4ae7e739fa
Santosh9696 <35653819+patilsp@users.noreply.github.com>
2019-01-10 15:56:45 +0530

    Create SwpingNumber.php

---
+++ SwpingNumber.php
@@ -0,0 +1,11 @@
+//Swaping number using third variable
+
+<?php  
+$a = 30;  
+$b = 45;  
+$third = $a;  
+$a = $b;  
+$b = $third;  
+echo "After swapping:<br><br>";  
+echo "a =".$a."  b=".$b;  
+?>  
